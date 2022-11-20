02b697464165575e499ae7a9ca64ec5bfcc95b05
ksky793 <k.sky793@gmail.com>
2022-11-20 16:04:15 +0100

    Dodano Searchbar z możliwością wyszukiwania
    - wyszukiwanie po naciśnięniu przycisku Szukaj
    - wyszukiwanie po naciśnięciu Enter

--- src/components/header/searchbar/Searchbar.js
+++ src/components/header/searchbar/Searchbar.js
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
 const Searchbar = () => {
-	return <div>searchbar</div>;
+	const [term, setTerm] = useState('');
+
+	const search = () => {
+		console.log(term);
+	};
+
+	return (
+		<div className='container input'>
+			<div className='d-flex'>
+				<input
+					className='form-control'
+					onKeyDown={(e) => e.key === 'Enter' && search()}
+					onChange={(e) => setTerm(e.target.value)}
+					placeholder='Szukaj...'
+					style={{ height: '50px' }}
+				></input>
+				<button className='btn btn-dark' onClick={search}>
+					Szukaj
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Searchbar;
