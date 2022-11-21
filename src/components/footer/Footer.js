57a29cd7cede7e9e5b7cafb60d43b4bc5010cdb7
ksky793 <k.sky793@gmail.com>
2022-11-21 14:28:52 +0100

    Dodano Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer id='sticky-footer' className='py-4 bg-dark text-white'>
+			<div className='container text-center'>
+				<p>Noclegi 2022</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
