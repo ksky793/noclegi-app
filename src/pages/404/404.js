031f526905fce0448ffe8e49b1131fc1e70f55a9
ksky793 <k.sky793@gmail.com>
2022-11-30 18:27:10 +0100

    Dodano stronę 404

---
+++ src/pages/404/404.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+	return (
+		<div className='mt-5 mb-5 text-center'>
+			<h2>404</h2>
+			<p>Nie znaleziono takiej strony</p>
+		</div>
+	);
+};
+
+export default NotFound;
