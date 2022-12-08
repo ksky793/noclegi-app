0dc368725deb447e0b92cd7826e4ba425f88ad64
ksky793 <k.sky793@gmail.com>
2022-12-08 02:04:57 +0100

    Zaktualizowano wyszukowanie hoteli
    - wyszukuje hotele, które maja ustawiony status widoczny

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,43 +2,32 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Hotels from '../components/hotels/Hotels';
 import LoadingButton from '../components/ui/LoadingIcon';
+import axios from '../axios';
+import { getArrayFromObject } from '../helpers/objects';
 
-const hotelsData = [
-	{
-		id: 1,
-		name: 'Pod Akcjami',
-		city: 'Warszawa',
-		rating: '8.5',
-		description: 'abcjdskjdfka',
-		image: 'img',
-	},
-	{
-		id: 2,
-		name: 'Wolind',
-		city: 'Białystok',
-		rating: '8.5',
-		description: 'abcjdskjdfka',
-		image: 'img',
-	},
-];
 const Search = () => {
 	const { term } = useParams();
 	const [hotels, setHotels] = useState([]);
 	const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		setTimeout(() => {
-			const newHotels = [...hotelsData].filter((hotel) =>
+	const search = async () => {
+		try {
+			const res = await axios.get('/hotels.json');
+			// chcemy wyświetlić hotele, które mają status widoczne = 1
+			const newHotels = getArrayFromObject(res).filter((hotel) =>
 				hotel.name.toLowerCase().includes(term.toLowerCase())
 			);
-			if (loading) {
-				setHotels(newHotels);
-				setLoading(false);
-			}
+			setLoading(false);
+			setHotels(newHotels);
+		} catch (e) {
+			console.log(e.response);
+			setLoading(false);
+		}
+	};
 
-			console.log(newHotels);
-		}, 1000);
-	}, []);
+	useEffect(() => {
+		search();
+	}, [term]);
 	return (
 		<section id='search-results' className='mt-5 mb-5'>
 			<h5>Wyniki wyszukiwania: {term}</h5>
